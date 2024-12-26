20aa0098bb47a9219e28a1e9155698b20f22fb45
Carlos Eduardo00 <carlosedusantoseduardo@gmail.com>
2024-12-26 16:02:22 -0300

    criação: integrando app com api

---
+++ src/components/ListItem/ListItem.jsx
@@ -0,0 +1,17 @@
+import './ListItem.css';
+
+function ListItem({ item }) {
+    const { name, sprites } = item;
+    const sprite = sprites.other["official-artwork"].front_default;
+
+    return (
+        <li className="list-item">
+            <div className="list-item-image-wrapper">
+                <img className="list-item-image" src={sprite} alt="Pokemon" />
+            </div>
+            <p>{name}</p>
+        </li>
+    )
+}
+
+export default ListItem;
